c5161e3625c2ea73ac198a0ae0b7730d36f0ba43
amyrobyn <amykrystosik@gmail.com>
2017-05-11 14:50:03 -0700

    import to redcap

--- redcap/import data to redcap.R
+++ redcap/import data to redcap.R
@@ -1,7 +1,12 @@
+library(redcapAPI)
 # Your REDCap issued token, I read mine from a text file
 Redcap.token <- readLines("C:/Users/amykr/Box Sync/Amy Krystosik's Files/Data Managment/redcap/ro1 lab results long/Redcap.token.txt") # Read API token from folder
 # REDCAp site API-URL, will most likely be the REDCap site where you normally login + api
 REDcap.URL  <- 'https://redcap.stanford.edu/api/'
 rcon <- redcapConnection(url=REDcap.URL, token=Redcap.token)
-df <- df[,colSums(is.na(df))<nrow(df)]
-glimpse(df)
+
+
+## S3 method for class 'redcapApiConnection'
+importRecords(rcon, df, overwriteBehavior = "normal", returnContent = c("count", "ids", "nothing"), 
+              returnData = FALSE, logfile = "", proj = NULL,
+              batch.size = -1)
