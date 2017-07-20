1986abf86d4e8b69c0ba3706132bc199b7a83cf8
amyrobyn <amykrystosik@gmail.com>
2017-07-20 11:48:56 -0700

    sharing with melisa her code

--- R01/redcap/melisa_aic.R
+++ R01/redcap/melisa_aic.R
@@ -1,8 +1,26 @@
+#install packages
+#attach libraries 
+
 setwd("C:/Users/amykr/Box Sync/Amy Krystosik's Files/melisa shah")
 
-melisa_aic<-load("R01CHIKVDENVProject_R_2017-07-20_1136.rda")
+#method downloading report from redcap
+R01_lab_results<-load("R01_lab_results.backup.rda")
+
+#download from api.
+
+#creating the cohort and village and visit variables for each visit. 
+R01_lab_results$id_cohort<-substr(R01_lab_results$person_id, 2, 2)
+table(R01_lab_results$id_cohort)
+R01_lab_results$id_city<-substr(R01_lab_results$person_id, 1, 1)
 
 
-aic<-R01_lab_results[which(R01_lab_results$id_visit > 0), c("person_id", "redcap_event_name","symptoms", "symptoms_aic", "id_cohort", "id_city", "id_visit")]
+#subsetting the data for just aic
+#change and put your vars here.
+aic<-R01_lab_results[which(R01_lab_results$ id_cohort=="F"), c("person_id", "redcap_event_name","symptoms", "symptoms_aic", "id_cohort", "id_city", "id_visit")]
+
+#describe 
+
+
+#exporting to csv
 f <- "aic_dummy_symptoms_de_identified.csv"
 write.csv(as.data.frame(aic), f )
