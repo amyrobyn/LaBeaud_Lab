407ba6f78057934cf320116afb0514b273bfb7bb
amyrobyn <amykrystosik@gmail.com>
2017-08-09 14:16:15 -0700

    send desiree the symptomatic vs asymptomatic denv aic cases

--- R01/redcap/split mc vars.R
+++ R01/redcap/split mc vars.R
@@ -1,5 +1,7 @@
 #install.packages(c("REDCapR", "mlr"))
 #install.packages(c("dummies"))
+library(dplyr)
+library(plyr)
 library(redcapAPI)
 library(REDCapR)
 
@@ -10,14 +12,23 @@ rcon <- redcapConnection(url=REDcap.URL, token=Redcap.token)
 
 #export data from redcap to R (must be connected via cisco VPN)
 #R01_lab_results <- redcap_read(redcap_uri  = REDcap.URL, token = Redcap.token, batch_size = 300)$data
-
 #R01_lab_results.backup<-R01_lab_results
 #save(R01_lab_results.backup,file="R01_lab_results.backup.rda")
+
 load("R01_lab_results.backup.rda")
 R01_lab_results<-R01_lab_results.backup
+R01_lab_results<- R01_lab_results[which(!is.na(R01_lab_results$redcap_event_name))  , ]
 
 R01_lab_results$id_cohort<-substr(R01_lab_results$person_id, 2, 2)
 R01_lab_results$id_city<-substr(R01_lab_results$person_id, 1, 1)
+
+n_distinct(R01_lab_results$person_id)
+table(aic_dummy_symptoms$id_cohort, aic_dummy_symptoms$redcap_event_name, exclude = NULL)
+
+table(aic_dummy_symptoms$id_cohort, exclude = NULL)
+table(aic_dummy_symptoms$redcap_event_name, exclude = NULL)
+
+
 R01_lab_results$id_visit<-as.integer(factor(R01_lab_results$redcap_event_name))
 R01_lab_results$id_visit<-R01_lab_results$id_visit-1
 symptoms <- R01_lab_results[, grepl("person_id|redcap_event|chikv_stfd_igg|chikv_kenya_igg|denv_kenya_igg|denv_stfd_igg|symptoms|id_c|id_v|visit_type", names(R01_lab_results))]
@@ -67,6 +78,7 @@ symptoms$all_symptoms<-paste(symptoms$symptoms, symptoms$symptoms_aic , sep=" ")
         symptom_sum<-rowSums(symptoms[, grep("\\baic_symptom", names(symptoms))])
         symptoms<-symptoms[ , grepl( "aic_symptom" , names(symptoms) ) ]
         symptoms$symptom_sum <- as.integer(rowSums(symptoms[ , grep("aic_symptom" , names(symptoms))]))
+        table(symptoms$symptom_sum, exclude=NULL)
         symptoms$symptomatic<-NA
         symptoms <- within(symptoms, symptomatic[symptoms$symptom_sum>0] <- 1)
         symptoms <- within(symptoms, symptomatic[symptoms$symptom_sum==0] <- 0)
@@ -74,53 +86,61 @@ symptoms$all_symptoms<-paste(symptoms$symptoms, symptoms$symptoms_aic , sep=" ")
 #how much of our acute DENV was symptomatic vs. mildly/asymptomatic, etc.
   table(symptoms$symptomatic, exclude=NULL)
 #export to box.
-setwd("C:/Users/amykr/Box Sync/Amy Krystosik's Files/Data Managment/reports")
 symptoms<-as.data.frame(cbind(ids, symptoms))
+
 
 table(symptoms$symptomatic, symptoms$redcap_event_name, symptoms$id_cohort, exclude=NULL)
 table(symptoms$symptomatic, symptoms$visit_type, symptoms$id_cohort, exclude=NULL)
 table(symptoms$symptomatic, symptoms$visit_type, symptoms$id_cohort, exclude=NULL)
 
-aic_symptoms<-subset(symptoms, id_cohort=="F")
-aic_symptoms <-aic_symptoms[!sapply(aic_symptoms, function (x) all(is.na(x) | x == ""))]
+aic_symptoms<-subset(symptoms, id_cohort!="C")
 
 #seroconversion
-  R01_lab_results<-R01_lab_results.backup
   seroconverter<-R01_lab_results[, grepl("person_id|redcap_event|ab_|bc_|cd_|de_|ef_|fg_|gh_", names(R01_lab_results))]
     seroconverter<-seroconverter[which(seroconverter$redcap_event_name=="patient_informatio_arm_1"),]
-    nameVec <- names(seroconverter)
     
-    v.names=c('denv_kenya_igg', 'chikv_kenya_igg', 'denv_stfd_igg',  'chikv_stfd_igg')
+    #order
+      seroconverter<-seroconverter[,order(colnames(seroconverter))]
+        
+      seroconverter<-seroconverter[order(-(grepl('gh_$', names(seroconverter)))+1L)]
+      seroconverter<-seroconverter[order(-(grepl('fg_$', names(seroconverter)))+1L)]
+      seroconverter<-seroconverter[order(-(grepl('ef_$', names(seroconverter)))+1L)]
+      seroconverter<-seroconverter[order(-(grepl('de_$', names(seroconverter)))+1L)]
+      seroconverter<-seroconverter[order(-(grepl('cd_$', names(seroconverter)))+1L)]
+      seroconverter<-seroconverter[order(-(grepl('bc_$', names(seroconverter)))+1L)]
+      seroconverter<-seroconverter[order(-(grepl('ab_$', names(seroconverter)))+1L)]
+  
+    nameVec <- names(seroconverter)
+    v.names=c('chikv_kenya_igg', 'chikv_stfd_igg', 'denv_kenya_igg', 'denv_stfd_igg')
     times = c("ab_", "bc_", "cd_", "de_", "ef_", "fg_", "gh_")    
-    seroconverter_long<-reshape(seroconverter, idvar = "person_id", varying = 3:30,  direction = "long", timevar = "visit", times=times, v.names=v.names)
-    seroconverter_long$visit
+    seroconverter_long<-reshape(seroconverter, idvar = "person_id", varying = 1:28,  direction = "long", timevar = "visit", times=times, v.names=v.names)
     seroconverter_long <- within(seroconverter_long, visit[visit=="ab_"] <- "visit_a_arm_1")
     seroconverter_long <- within(seroconverter_long, visit[visit=="bc_"] <- "visit_b_arm_1")
-    seroconverter_long <- within(seroconverter_long, visit[visit=="cd_"] <- "visit_d_arm_1")
+    seroconverter_long <- within(seroconverter_long, visit[visit=="cd_"] <- "visit_c_arm_1")
     seroconverter_long <- within(seroconverter_long, visit[visit=="de_"] <- "visit_d_arm_1")
     seroconverter_long <- within(seroconverter_long, visit[visit=="ef_"] <- "visit_e_arm_1")
     seroconverter_long <- within(seroconverter_long, visit[visit=="fg_"] <- "visit_f_arm_1")
     seroconverter_long <- within(seroconverter_long, visit[visit=="gh_"] <- "visit_g_arm_1")
     seroconverter_long$redcap_event_name<-seroconverter_long$visit
-    seroconverter_long<-rename(seroconverter_long, c("denv_kenya_igg" = "seroc_denv_kenya_igg"))
-    seroconverter_long<-rename(seroconverter_long, c("chikv_kenya_igg" = "seroc_chikv_kenya_igg"))
-    seroconverter_long<-rename(seroconverter_long, c("denv_stfd_igg" = "seroc_denv_stfd_igg"))
-    seroconverter_long<-rename(seroconverter_long, c("chikv_stfd_igg" = "seroc_chikv_stfd_igg"))
+    names(seroconverter_long)[names(seroconverter_long) == 'denv_kenya_igg'] <- 'seroc_denv_kenya_igg'
+    names(seroconverter_long)[names(seroconverter_long) == 'chikv_kenya_igg'] <- 'seroc_chikv_kenya_igg'
+    names(seroconverter_long)[names(seroconverter_long) == 'denv_stfd_igg'] <- 'seroc_denv_stfd_igg'
+    names(seroconverter_long)[names(seroconverter_long) == 'chikv_stfd_igg'] <- 'seroc_chikv_stfd_igg'
+    
     head(seroconverter_long)
 
 #merge symptoms to redcap data
   aic_dummy_symptoms <- merge(seroconverter_long, aic_symptoms,  by=c("person_id", "redcap_event_name"), all = TRUE)
-  aic_dummy_symptoms <-aic_dummy_symptoms[!sapply(aic_dummy_symptoms, function (x) all(is.na(x) | x == ""))]
   aic_dummy_symptoms<-aic_dummy_symptoms[, grepl("person_id|redcap|visit|symptom|seroc|temp", names(aic_dummy_symptoms))]
 #merge pcr results
   pcr<-R01_lab_results[, grepl("person_id|redcap_event_name|result_pcr", names(R01_lab_results))]
   aic_dummy_symptoms <- merge(pcr, aic_dummy_symptoms,  by=c("person_id", "redcap_event_name"), all = TRUE)
-  aic_dummy_symptoms <-aic_dummy_symptoms[!sapply(aic_dummy_symptoms, function (x) all(is.na(x) | x == ""))]
+  names(aic_dummy_symptoms)[names(aic_dummy_symptoms) == 'redcap_event_name'] <- 'redcap_event'
   
 #double check to de-identify data
   identifiers<-grep("name|gps", names(aic_dummy_symptoms), value = TRUE)
   aic_dummy_symptoms<-aic_dummy_symptoms[ , !(names(aic_dummy_symptoms) %in% identifiers)]
-  aic_dummy_symptoms<-rename(aic_dummy_symptoms, c("visit" = "redcap_event_name"))
+  
 #export to csv
   setwd("C:/Users/amykr/Box Sync/Amy Krystosik's Files/Data Managment/redcap/ro1 lab results long")
   f <- "aic_dummy_symptoms_de_identified.csv"
@@ -128,33 +148,83 @@ aic_symptoms <-aic_symptoms[!sapply(aic_symptoms, function (x) all(is.na(x) | x 
 
 #analysis
   attach(aic_dummy_symptoms)
-  table(aic_dummy_symptoms$seroc_chikv_kenya_igg, seroc_chikv_stfd_igg, exclude=NULL)
   table(aic_dummy_symptoms$seroc_denv_kenya_igg, aic_dummy_symptoms$seroc_denv_stfd_igg, exclude=NULL)
+  
+  aic_dummy_symptoms$id_cohort<-substr(aic_dummy_symptoms$person_id, 2, 2)
+  aic_dummy_symptoms$aic_dummy_symptoms<-substr(aic_dummy_symptoms$person_id, 1, 1)
+  
+  n_distinct(aic_dummy_symptoms$person_id)
+  table(R01_lab_results$cohort)
+  table(R01_lab_results$id_cohort, R01_lab_results$redcap_event_name )
+  5204+3918
+  5188+16+13 +3918
 
 #kenya denv igg seroconverters or PCR positives as infected.
   aic_dummy_symptoms$infected_denv_kenya[aic_dummy_symptoms$seroc_denv_kenya_igg==1|aic_dummy_symptoms$result_pcr_denv_kenya==1|aic_dummy_symptoms$result_pcr_denv_stfd==1]<-1
-  table(aic_dummy_symptoms$infected_denv_kenya)  
+  table(aic_dummy_symptoms$infected_denv_kenya,aic_dummy_symptoms$id_cohort)  
 #kenya chikv igg seroconverters or PCR positives as infected.
   aic_dummy_symptoms$infected_chikv_kenya[aic_dummy_symptoms$seroc_chikv_kenya_igg==1|aic_dummy_symptoms$result_pcr_chikv_kenya==1]<-1
-  table(aic_dummy_symptoms$infected_chikv_kenya)  
+  table(aic_dummy_symptoms$infected_chikv_kenya, aic_dummy_symptoms$id_cohort)  
 #stfd denv igg seroconverters or PCR positives as infected.
   aic_dummy_symptoms$infected_denv_stfd[aic_dummy_symptoms$seroc_denv_stfd_igg==1|aic_dummy_symptoms$result_pcr_denv_kenya==1|aic_dummy_symptoms$result_pcr_denv_stfd==1]<-1
-  table(aic_dummy_symptoms$infected_denv_stfd)  
+  table(aic_dummy_symptoms$infected_denv_stfd, aic_dummy_symptoms$id_cohort)  
 #stfd chikv igg seroconverters or PCR positives as infected.
   aic_dummy_symptoms$infected_chikv_stfd[aic_dummy_symptoms$seroc_chikv_stfd_igg==1|aic_dummy_symptoms$result_pcr_chikv_kenya==1]<-1
-  table(aic_dummy_symptoms$infected_chikv_stfd)  
+  table(aic_dummy_symptoms$infected_chikv_stfd, aic_dummy_symptoms$id_cohort)  
+  table(aic_dummy_symptoms$symptomatic)  
+
+#stanford infected denv aic
+  #symptom sum histogram for aic
+  infected_pcr_denv_stfd_igg<-aic_dummy_symptoms[which(aic_dummy_symptoms$infected_denv_stfd==1),]
+  tiff(file = "AIC DENV Infected (Stanford IgG, all PCR).tiff", width = 3200, height = 3200, units = "px", res = 800)
+  hist(infected_pcr_denv_stfd_igg$symptom_sum, breaks=50, main = "AIC DENV Infected (Stanford IgG, all PCR)", xlab = "Number of Symptoms Reported at acute", freq=TRUE)
+  dev.off()
+  #cohort
+  infected_pcr_denv_stfd_igg$id_cohort<-substr(infected_pcr_denv_stfd_igg$person_id, 2, 2)
+  table(infected_pcr_denv_stfd_igg$id_cohort , exclude = NULL)
+  #symptomatic aic
+  table(aic_dummy_symptoms$symptomatic, aic_dummy_symptoms$infected_denv_stfd)
   
-seroc_chikv_kenya_igg_symptomatic<-table(aic_dummy_symptoms$symptomatic, aic_dummy_symptoms$redcap_event_name, aic_dummy_symptoms$infected_chikv_kenya)
-seroc_chikv_kenya_igg_symptom_sum<-table( aic_dummy_symptoms$symptom_sum, aic_dummy_symptoms$redcap_event_name, aic_dummy_symptoms$infected_chikv_kenya)
 
-seroc_denv_kenya_igg_symptomatic<-table( aic_dummy_symptoms$symptomatic, aic_dummy_symptoms$redcap_event_name, aic_dummy_symptoms$infected_denv_kenya)
-seroc_denv_kenya_igg_symptom_sum<-table( aic_dummy_symptoms$symptom_sum, aic_dummy_symptoms$redcap_event_name, aic_dummy_symptoms$infected_denv_kenya)
+#kenya infected denv aic
+  #symptom sum for aic
+  infected_pcr_denv_kenya_igg<-aic_dummy_symptoms[which(aic_dummy_symptoms$infected_denv_kenya==1),]
+  tiff(file = "AIC DENV Infected (Kenya IgG, all PCR).tiff", width = 3200, height = 3200, units = "px", res = 800)
+  hist(infected_pcr_denv_kenya_igg$symptom_sum, breaks=50, main = "AIC DENV Infected (Kenya IgG, all PCR)", xlab = "Number of Symptoms Reported at acute", freq=TRUE)
+  dev.off()
+  #cohort
+  infected_pcr_denv_kenya_igg$id_cohort<-substr(infected_pcr_denv_kenya_igg$person_id, 2, 2)
+  table(infected_pcr_denv_kenya_igg$id_cohort , exclude = NULL)
+  #symptomatic for aic
+  table(aic_dummy_symptoms$symptomatic, aic_dummy_symptoms$infected_denv_kenya)
+  
+#stanford infected chikv aic
+  #symptom sum histogram for aic
+  infected_pcr_chikv_stfd_igg<-aic_dummy_symptoms[which(aic_dummy_symptoms$infected_chikv_stfd==1),]
+  tiff(file = "AIC chikv Infected (Stanford IgG, all PCR).tiff", width = 3200, height = 3200, units = "px", res = 800)
+  hist(infected_pcr_chikv_stfd_igg$symptom_sum, breaks=50, main = "AIC chikv Infected (Stanford IgG, all PCR)", xlab = "Number of Symptoms Reported at acute", freq=TRUE)
+  dev.off()
+  #cohort
+  infected_pcr_chikv_stfd_igg$id_cohort<-substr(infected_pcr_chikv_stfd_igg$person_id, 2, 2)
+  table(infected_pcr_chikv_stfd_igg$id_cohort , exclude = NULL)
+  #symptomatic aic
+  table(aic_dummy_symptoms$symptomatic, aic_dummy_symptoms$infected_chikv_stfd)
+  
+  
+#kenya infected chikv aic
+  #symptom sum for aic
+  infected_pcr_chikv_kenya_igg<-aic_dummy_symptoms[which(aic_dummy_symptoms$infected_chikv_kenya==1),]
 
-seroc_chikv_stfd_igg_symptomatic<-table( aic_dummy_symptoms$symptomatic, aic_dummy_symptoms$redcap_event_name, aic_dummy_symptoms$infected_chikv_stfd)
-seroc_chikv_stfd_igg_symptom_sum<-table( aic_dummy_symptoms$symptom_sum, aic_dummy_symptoms$redcap_event_name, aic_dummy_symptoms$infected_chikv_stfd)
-
-seroc_denv_stfd_igg_symptomatic<-table( aic_dummy_symptoms$symptomatic, aic_dummy_symptoms$redcap_event_name, aic_dummy_symptoms$infected_denv_stfd)
-seroc_denv_stfd_igg_symptom_sum<-table( aic_dummy_symptoms$symptom_sum, aic_dummy_symptoms$redcap_event_name, aic_dummy_symptoms$infected_denv_stfd)
-
-hist(seroc_denv_stfd_igg_symptom_sum, breaks=50)
-hist(seroc_denv_kenya_igg_symptom_sum, breaks=50)
+  tiff(file = "AIC chikv Infected (Kenya IgG, all PCR).tiff", width = 3200, height = 3200, units = "px", res = 800)
+  hist(infected_pcr_chikv_kenya_igg$symptom_sum, breaks=50, main = "AIC chikv Infected (Kenya IgG, all PCR)", xlab = "Number of Symptoms Reported at acute", freq=TRUE)
+  dev.off()
+  
+  #cohort
+  infected_pcr_chikv_kenya_igg$id_cohort<-substr(infected_pcr_chikv_kenya_igg$person_id, 2, 2)
+  table(infected_pcr_chikv_kenya_igg$id_cohort , exclude = NULL)
+  #symptomatic for aic
+  table(aic_dummy_symptoms$symptomatic, aic_dummy_symptoms$infected_chikv_kenya)
+  
+##survival analysis
+  
+  
