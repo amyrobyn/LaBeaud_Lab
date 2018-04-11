8d9ab59cab839cc9ddb6d63d6ad7df811cb2b1fa
amyrobyn <amykrystosik@gmail.com>
2018-04-11 15:50:56 -0700

    send melisa graphs and code for malaria model

--- R01/Melisa Shah/melisa logit-cleanv2.R
+++ R01/Melisa Shah/melisa logit-cleanv2.R
@@ -22,25 +22,25 @@ plot(temp.long$date_collected, round(temp.long$meanTemp), exclude=NULL)
 
 
 # import climate data from redcap -----------------------------------------
-library(redcapAPI)
-library(REDCapR)
-setwd("C:/Users/amykr/Box Sync/Amy Krystosik's Files/vector")
-Redcap.token <- readLines("api.key.txt") # Read API token from folder
-REDcap.URL  <- 'https://redcap.stanford.edu/api/'
-rcon <- redcapConnection(url=REDcap.URL, token=Redcap.token)
-#climate <- redcap_read(redcap_uri  = REDcap.URL, token = Redcap.token, batch_size = 100)$data#export data from redcap to R 
-#save backup from today
-currentDate <- Sys.Date() 
-FileName <- paste("vector_climate",currentDate,".rda",sep=" ") 
-  save(climate,file=FileName)
-
-# or 2. load saved climate -----------------------------------------------------------------
+  library(redcapAPI)
+  library(REDCapR)
+  setwd("C:/Users/amykr/Box Sync/Amy Krystosik's Files/vector")
+  Redcap.token <- readLines("api.key.txt") # Read API token from folder
+  REDcap.URL  <- 'https://redcap.stanford.edu/api/'
+  rcon <- redcapConnection(url=REDcap.URL, token=Redcap.token)
+#  climate <- redcap_read(redcap_uri  = REDcap.URL, token = Redcap.token, batch_size = 100)$data#export data from redcap to R 
+  #save backup from today
   currentDate <- Sys.Date() 
   FileName <- paste("vector_climate",currentDate,".rda",sep=" ") 
-#load most recent backup
-#  load(FileName)
-#or 3. import from redcap export  -----------------------------------------------------------------
-  climate<-  read.csv("C:/Users/amykr/Box Sync/Amy Krystosik's Files/melisa shah/climate/20180411111858_pid11751_MeJzB3.csv")
+#    save(climate,file=FileName)
+  
+    # or 2. load saved climate -----------------------------------------------------------------
+            currentDate <- Sys.Date() 
+            FileName <- paste("vector_climate",currentDate,".rda",sep=" ") 
+          #load most recent backup
+          #  load(FileName)
+    #or 3. import from redcap export  -----------------------------------------------------------------
+        #climate<-  read.csv("C:/Users/amykr/Box Sync/Amy Krystosik's Files/melisa shah/climate/20180411111858_pid11751_MeJzB3.csv")
 # rain climate -----------------------------------------------------------------
 library(zoo)
 library(lubridate)
@@ -84,18 +84,12 @@ climate.c <- merge(all.dates.frame, climate.c, all.x=T, by=c("date_collected"))
 climate<-rbind(climate.c,climate.k,climate.m,climate.u)
 rain<-climate[,c("date_collected","daily_rainfall","rainfall_hobo","site","daily_rainfall_long_term_mean")]
 
-#rain <- within (rain, daily_rainfall[rain$daily_rainfall>120] <-NA)
 plot(rain$date_collected, round(rain$daily_rainfall), exclude=NULL)
-
 temp.long <- merge(all.dates.frame, temp.long, all.x=T, by=c("date_collected"))
 climate<-merge(rain, temp.long, by = c("date_collected", "site") )
 
-table(!is.na(climate$daily_rainfall))
-climate <- within (climate, daily_rainfall[rain$daily_rainfall=="NaN"] <-NA)
 
-climate$month_collected <- as.yearmon(climate$date_collected)
-table(round(climate$daily_rainfall,climate$month_collected), exclude = NULL)
-
+# make rolling means and sums over time -----------------------------------
 library(dplyr)
 climate = climate %>%
   group_by(site) %>%
@@ -104,11 +98,10 @@ climate = climate %>%
     temp_mean_30 = rollmean(x = meanTemp, 30, align = "right", fill = NA),
     rainfall_sum_30 = rollsum(x = daily_rainfall, 30, align = "right", fill = NA)
   )
-
-table(round(climate$temp_mean_30), climate$site, exclude=NA)
+# plot rolling means and sums over time -----------------------------------
 library(ggplot2)
-ggplot (climate, aes (x = date_collected, y = temp_mean_30, colour = site)) + geom_point ()
-ggplot (climate, aes (x = date_collected, y = rainfall_sum_30, colour = site)) + geom_point ()
+ggplot (climate, aes (x = date_collected, y = temp_mean_30, colour = site)) +geom_line(linetype = "solid",size=2) +scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +theme(axis.text.x=element_text(angle=60, hjust=1),legend.position="none",text = element_text(size = 20)) + facet_grid(site ~ .)+xlab("Month-Year") + ylab("Average Temperature (C) in last 30 days") 
+ggplot (climate, aes (x = date_collected, y = rainfall_sum_30, colour = site)) +geom_line(linetype = "solid",size=2) +scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +theme(axis.text.x=element_text(angle=60, hjust=1),legend.position="none",text = element_text(size = 20)) + facet_grid(site ~ .)+xlab("Month-Year") + ylab("Cummulative Precipitation (mm) in last 30 days") 
 
 # malaria climate merge-----------------------------------------------------------------
 aicmalaria <- readRDS("C:/Users/amykr/Box Sync/Amy Krystosik's Files/melisa shah/aicmalaria.rds")
@@ -139,7 +132,7 @@ malaria_climate_k<-malaria_climate[which(malaria_climate$id_site_A=="Kisumu"),]
 
 # table one ---------------------------------------------------------------
 library(tableone)
-vars<-c("ses_sum","rainfall_sum_30","temp_mean_30","rainfall_hobo","aic_calculated_age_A",  "temp_A", "roof_type_A", "latrine_type_A", "floor_type_A", "drinking_water_source_A", "number_windows_A", "gender_aic_A", "fever_contact_A", "mosquito_bites_aic_A", "mosquito_net_aic_A")
+vars<-c("ses_sum","rainfall_sum_30","temp_mean_30","aic_calculated_age_A",  "temp_A", "roof_type_A", "latrine_type_A", "floor_type_A", "drinking_water_source_A", "number_windows_A", "gender_aic_A", "fever_contact_A", "mosquito_bites_aic_A", "mosquito_net_aic_A")
 factorVars<-c("roof_type_A", "latrine_type_A", "floor_type_A", "drinking_water_source_A", "gender_aic_A", "fever_contact_A", "mosquito_bites_aic_A", "mosquito_net_aic_A")
 
 tableOne_mal_c <- CreateTableOne(vars = vars, factorVars = factorVars, strata = "result_microscopy_malaria_kenya_A", data = malaria_climate_c)
@@ -149,10 +142,6 @@ tableOne_mal_u <- CreateTableOne(vars = vars, factorVars = factorVars, strata = 
 
 tableOne_site <- CreateTableOne(vars = vars, factorVars = factorVars, strata = "id_site_A", data = malaria_climate)
 
-# scatter plot ------------------------------------------------------------
-my_cols <- c("#00AFBB", "#E7B800")  
-pairs(na.omit(malaria_climate[vars]), pch = 19,  cex = 0.5, col = my_cols[aicmalaria$result_microscopy_malaria_kenya_A], lower.panel=NULL)
-cor(na.omit(malaria_climate[vars]))
 # non-linear temperature option 1. splines -------------------------------------------------------
 #install.packages("splines")
 library("splines")
@@ -174,31 +163,45 @@ malaria_climate$id_site_A<-as.factor(malaria_climate$id_site_A)
 bs(malaria_climate$temp_mean_30, df = 3)
 bs(malaria_climate$rainfall_sum_30, df = 4)
 
+# correlation  ------------------------------------------------------------
+corrvars<-c("result_microscopy_malaria_kenya_A","temp_mean_30", "rainfall_sum_30", "agecat_A" ,  "fever_contact_A",  "mosquito_bites_aic_A" ,"id_site_A", "gender_aic_A","ses_sum")
+my_cols <- c("#00AFBB", "#E7B800")  
+pairs(na.omit(malaria_climate[corrvars]), pch = 19,  cex = 0.5, col = my_cols[aicmalaria$result_microscopy_malaria_kenya_A], lower.panel=NULL)
+cor(na.omit(malaria_climate[vars]))
+
 # random intercept for site -----------------------------------------------------------------
 library(lmerTest)
 library(lme4)
-class(malaria_climate$id_site_A)
-summary(spline.malaria.random <- lmer(result_microscopy_malaria_kenya_A ~ reportcough_A+reportdiarrhea_A+reportnv_A+reportjoint_A+bs(temp_mean_30, df = 3) + bs(rainfall_sum_30, df = 4) + as.factor(agecat_A) +   fever_contact_A + mosquito_bites_aic_A +  (1|id_site_A)+gender_aic_A, data = malaria_climate))
+summary(spline.malaria.random <- lmer(result_microscopy_malaria_kenya_A ~ bs(temp_mean_30, df = 3) + bs(rainfall_sum_30, df = 4) + agecat_A +   fever_contact_A + mosquito_bites_aic_A +  (1|id_site_A)+gender_aic_A + ses_sum, data = malaria_climate))
 anova(spline.malaria.random)
-exp(confint(spline.malaria.random, method="boot", parallel="multicore", ncpus=4))
+#exp(confint(spline.malaria.random, method="boot", parallel="multicore", ncpus=4))
 
 # all sites -----------------------------------------------------------------
-summary(spline.malaria <- lm(result_microscopy_malaria_kenya_A ~ reportcough_A+reportdiarrhea_A+reportnv_A+reportjoint_A+bs(temp_mean_30, df = 3) +  bs(rainfall_sum_30, df = 4) + agecat_A +  fever_contact_A + mosquito_bites_aic_A + id_site_A + gender_aic_A, data = malaria_climate))
-table(malaria_climate$result_microscopy_malaria_kenya_A, round(malaria_climate$rainfall_sum_30), exclude=NULL)
+summary(spline.malaria <- lm(result_microscopy_malaria_kenya_A ~ bs(temp_mean_30, df = 3) + bs(rainfall_sum_30, df = 4) + agecat_A +  fever_contact_A + mosquito_bites_aic_A + id_site_A + gender_aic_A +ses_sum, data = malaria_climate))
 exp(cbind(OR = coef(spline.malaria), confint(spline.malaria)))
-# by site -----------------------------------------------------------------
-summary(spline.malaria.c <- lm(result_microscopy_malaria_kenya_A ~ reportcough_A+reportdiarrhea_A+reportnv_A+reportjoint_A+bs(temp_mean_30, df = 3) + rainfall_sum_30 + as.factor(agecat_A) +   fever_contact_A + mosquito_bites_aic_A  + gender_aic_A, data = malaria_climate_c))
-exp(cbind(OR = coef(spline.malaria.c), confint(spline.malaria.c)))
-summary(spline.malaria.k <- lm(result_microscopy_malaria_kenya_A ~ reportcough_A+reportdiarrhea_A+reportnv_A+reportjoint_A+bs(temp_mean_30, df = 3) + rainfall_sum_30  + as.factor(agecat_A) + fever_contact_A + mosquito_bites_aic_A +  gender_aic_A, data = malaria_climate_k))
-exp(cbind(OR = coef(spline.malaria.k), confint(spline.malaria.k)))
-summary(spline.malaria.m <- lm(result_microscopy_malaria_kenya_A ~ reportcough_A+reportdiarrhea_A+reportnv_A+reportjoint_A+bs(temp_mean_30, df = 3) + rainfall_sum_30 + as.factor(agecat_A) + fever_contact_A + mosquito_bites_aic_A +  gender_aic_A, data = malaria_climate_m))
-exp(cbind(OR = coef(spline.malaria.m), confint(spline.malaria.m)))
-summary(spline.malaria.u <- lm(result_microscopy_malaria_kenya_A ~ reportcough_A+reportdiarrhea_A+reportnv_A+reportjoint_A+ rainfall_sum_30 + as.factor(agecat_A) + fever_contact_A + mosquito_bites_aic_A +  gender_aic_A, data = malaria_climate_u))
-exp(cbind(OR = coef(spline.malaria.u), confint(spline.malaria.u)))
+anova(spline.malaria)
 
-#hold all effects constant except temp
-  plot(effects::Effect(focal.predictors = c("temp_mean_30"), mod = spline.malaria, 
-            xlevels = list(temp_mean_hobo.30 = 22:31)), rug = FALSE)
-  plot(effects::Effect(focal.predictors = c("rainfall_sum_30"), mod = spline.malaria, 
-                       xlevels = list(rainfall_sum_30 = 0:442)), rug = FALSE)
+range(malaria_climate$temp_mean_30)
+range(malaria_climate$temp_mean_30)
+plot(effects::Effect(focal.predictors = c("rainfall_sum_30"), mod = spline.malaria, xlevels = list(rainfall_sum_30 = 1:506)), rug = FALSE, main="Precipitation Effect on Malaria Transmission" ,ylab="Probablity of Plasmodium Positive Microscopy", xlab="Cummulative Precipitation (mm) in last 30 days")
+plot(effects::Effect(focal.predictors = c("temp_mean_30"), mod = spline.malaria, xlevels = list(temp_mean_30 = 22.6:31.02)), rug = FALSE, main="Temperature Effect on Malaria Transmission" ,ylab="Probablity of Plasmodium Positive Microscopy", xlab="Average Temperature (C) in last 30 days")
+
+# by site -----------------------------------------------------------------
+summary(spline.malaria.c <- lm(result_microscopy_malaria_kenya_A ~ bs(temp_mean_30, df = 3) + bs(rainfall_sum_30, df = 4) + agecat_A +   fever_contact_A + mosquito_bites_aic_A  + gender_aic_A + ses_sum, data = malaria_climate_c))
+anova(spline.malaria.c)
+exp(cbind(OR = coef(spline.malaria.c), confint(spline.malaria.c)))
+
+summary(spline.malaria.k <- lm(result_microscopy_malaria_kenya_A ~ bs(temp_mean_30, df = 3) + bs(rainfall_sum_30, df = 4) + agecat_A + fever_contact_A + mosquito_bites_aic_A +  gender_aic_A + ses_sum, data = malaria_climate_k))
+exp(cbind(OR = coef(spline.malaria.k), confint(spline.malaria.k)))
+anova(spline.malaria.k)
+
+summary(spline.malaria.m <- lm(result_microscopy_malaria_kenya_A ~ bs(temp_mean_30, df = 3) + bs(rainfall_sum_30, df = 4) + agecat_A + fever_contact_A + mosquito_bites_aic_A +  gender_aic_A +ses_sum , data = malaria_climate_m))
+exp(cbind(OR = coef(spline.malaria.m), confint(spline.malaria.m)))
+anova(spline.malaria.m)
+
+summary(spline.malaria.u <- lm(result_microscopy_malaria_kenya_A ~ bs(temp_mean_30, df = 3) + bs(rainfall_sum_30, df = 4) +agecat_A + fever_contact_A + mosquito_bites_aic_A +  gender_aic_A + ses_sum, data = malaria_climate_u))
+exp(cbind(OR = coef(spline.malaria.u), confint(spline.malaria.u)))
+anova(spline.malaria.u)
+
+# save merged dataset.  ---------------------------------------------------
 saveRDS(malaria_climate, file="malaria_climate.rds")
