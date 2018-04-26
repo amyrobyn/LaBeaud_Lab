98ddecc311ae8ffc8ecca1972f2ce6cf27bcf966
amyrobyn <amykrystosik@gmail.com>
2018-04-25 18:21:09 -0700

    zika abstract

--- brazil/brazil sinan hosp surv.R
+++ brazil/brazil sinan hosp surv.R
@@ -44,30 +44,30 @@ cases_week_df$date<-as.Date(cases_week_df$Var1)
 cases_week_df$neighborhood<-as.factor(cases_week_df$Var2)
 
 p<-ggplot(cases_week_df, aes (x = date, y = Freq, color=neighborhood)) +
-    scale_x_date(date_breaks = "3 months", date_labels =  "%b %Y") +
-    theme(axis.text.x=element_text(angle=60, hjust=1),text = element_text(size = 20))+
-    xlab("Date of notification") + ylab("DENV Cases reported") +
-    #geom_line(size=2) + 
-    geom_bar(stat="identity", size =2) + 
-    #facet_grid(neighborhood ~ ., scales = "free")+
-    theme(strip.text.y = element_text(angle = 0))
-    scaleFUN <- function(x) sprintf("%.0f", x)
-    
-  tiff("cases by neighborhood over time.tif", height = 18, width = 30, units = 'cm', compression = "lzw", res = 600)
-      p + scale_y_continuous(labels=scaleFUN)
-  dev.off()
+ scale_x_date(date_breaks = "3 months", date_labels = "%b %Y") +
+ theme(axis.text.x=element_text(angle=60, hjust=1),text = element_text(size = 20))+
+ xlab("Date of notification") + ylab("DENV Cases reported") +
+ #geom_line(size=2) + 
+ geom_bar(stat="identity", size =2) + 
+ #facet_grid(neighborhood ~ ., scales = "free")+
+ theme(strip.text.y = element_text(angle = 0))
+ scaleFUN <- function(x) sprintf("%.0f", x)
+ 
+ tiff("cases by neighborhood over time.tif", height = 18, width = 30, units = 'cm', compression = "lzw", res = 600)
+ p + scale_y_continuous(labels=scaleFUN)
+ dev.off()
 # cases over neighorhood --------------------------------------------------
-  
+ 
 p2<-ggplot(cases_week_df, aes (x = neighborhood, y = Freq, color=date)) +
-      #scale_x_date(date_breaks = "3 months", date_labels =  "%b %Y") +
-      theme(axis.text.x=element_text(angle=60, hjust=1),text = element_text(size = 20))+
-      xlab("Neighorhood") + ylab("DENV Cases reported") +
-      #geom_line(size=2) + 
-      geom_bar(stat="identity", size =2) + 
-      #facet_grid(neighborhood ~ ., scales = "free")+
-      theme(strip.text.y = element_text(angle = 0))
+ #scale_x_date(date_breaks = "3 months", date_labels = "%b %Y") +
+ theme(axis.text.x=element_text(angle=60, hjust=1),text = element_text(size = 20))+
+ xlab("Neighorhood") + ylab("DENV Cases reported") +
+ #geom_line(size=2) + 
+ geom_bar(stat="identity", size =2) + 
+ #facet_grid(neighborhood ~ ., scales = "free")+
+ theme(strip.text.y = element_text(angle = 0))
 tiff("cases over neighborhood by year.tif", height = 18, width = 40, units = 'cm', compression = "lzw", res = 600)
-  p2 + scale_y_continuous(labels=scaleFUN)
+ p2 + scale_y_continuous(labels=scaleFUN)
 dev.off()
 # cases over time by serology ---------------------------------------------
 cases_week_df<-as.data.frame(table(denv$date, denv$RESUL_SORO))
@@ -76,15 +76,15 @@ cases_week_df$date<-as.Date(cases_week_df$Var1)
 cases_week_df$serology<-as.factor(cases_week_df$Var2)
 
 p3<-ggplot(cases_week_df, aes (x = date, y = Freq, color=serology)) +
-      scale_x_date(date_breaks = "3 months", date_labels =  "%b %Y") +
-      theme(axis.text.x=element_text(angle=60, hjust=1),text = element_text(size = 20))+
-      xlab("Date of notification") + ylab("DENV Cases reported") +
-      #geom_line(size=2) + 
-      geom_bar(stat="identity", size =2) + 
-      #facet_grid(neighborhood ~ ., scales = "free")+
-      theme(strip.text.y = element_text(angle = 0))
+ scale_x_date(date_breaks = "3 months", date_labels = "%b %Y") +
+ theme(axis.text.x=element_text(angle=60, hjust=1),text = element_text(size = 20))+
+ xlab("Date of notification") + ylab("DENV Cases reported") +
+ #geom_line(size=2) + 
+ geom_bar(stat="identity", size =2) + 
+ #facet_grid(neighborhood ~ ., scales = "free")+
+ theme(strip.text.y = element_text(angle = 0))
 tiff("cases over time by serology.tif", height = 18, width = 40, units = 'cm', compression = "lzw", res = 600)
-    p3 + scale_y_continuous(labels=scaleFUN)
+ p3 + scale_y_continuous(labels=scaleFUN)
 dev.off()
 
 # cases over time by Sex ---------------------------------------------
@@ -94,21 +94,24 @@ cases_week_df$sex<-as.factor(cases_week_df$Var2)
 plot(cases_week_df$date, cases_week_df$Freq)
 
 p4<-ggplot(cases_week_df, aes (x = date, y = Freq, color=sex)) +
-      scale_x_date(date_breaks = "3 months", date_labels =  "%b %Y") +
-      theme(axis.text.x=element_text(angle=60, hjust=1),text = element_text(size = 20))+
-      xlab("Date of notification") + ylab("DENV Cases reported") +
-      #geom_line(size=2) + 
-      geom_bar(stat="identity", size =2,position='dodge') + 
-      #facet_grid(neighborhood ~ ., scales = "free")+
-      theme(strip.text.y = element_text(angle = 0))
+ scale_x_date(date_breaks = "3 months", date_labels = "%b %Y") +
+ theme(axis.text.x=element_text(angle=60, hjust=1),text = element_text(size = 20))+
+ xlab("Date of notification") + ylab("DENV Cases reported") +
+ #geom_line(size=2) + 
+ geom_bar(stat="identity", size =2,position='dodge') + 
+ #facet_grid(neighborhood ~ ., scales = "free")+
+ theme(strip.text.y = element_text(angle = 0))
 tiff("cases over time by sex.Tif", height = 18, width = 40, units = 'cm', compression = "lzw", res = 600)
-    p4 + scale_y_continuous(labels=scaleFUN)
+ p4 + scale_y_continuous(labels=scaleFUN)
 dev.off()
-    
+ 
 # table one ------------------------------------------------
 library(tableone)
-vars<-colnames( denv )
-factorVars <- vars
-tableOne <- CreateTableOne(vars = vars, strata = "year", data = denv)
+denv <- as.data.frame(unclass(denv))
+table(denv$year)
+
+vars<-c("SG_UF_NOT","ID_MUNICIP","ID_REGIONA","ID_UNIDADE","CS_SEXO","CS_GESTANT","CS_RACA","CS_ESCOL_N","ID_DISTRIT","ID_BAIRRO","ID_GEO1","NU_DDD_TEL","CS_ZONA","FEBRE","MIALGIA","CEFALEIA","NAUSEA","EXANTEMA","DOR_COSTAS","CONJUNTVIT","ARTRITE","ARTRALGIA","PETEQUIA_N","LEUCOPENIA","LACO","DOR_RETRO","DIABETES","HEMATOLOG","HEPATOPAT","RENAL", "HIPERTENSA","ACIDO_PEPT","AUTO_IMUNE","RES_CHIKS1","RES_CHIKS2","RESUL_PRNT","RESUL_SORO","RESUL_NS1","RESUL_VI_N","RESUL_PCR_","SOROTIPO","HISTOPA_N","IMUNOH_N","HOSPITALIZ","MUNICIPIO","HOSPITAL", "DDD_HOSP","TPAUTOCTO","COUFINF","COPAISINF","CO_BAINF","CLASSI_FIN","CRITERIO","DOENCA_TRA","EVOLUCAO","ALRM_HIPOT","ALRM_PLAQ","ALRM_VOM","ALRM_SANG","ALRM_HEMAT","ALRM_ABDOM","ALRM_LETAR","ALRM_HEPAT","ALRM_LIQ","GRAV_PULSO","GRAV_CONV","GRAV_ENCH","GRAV_INSUF","GRAV_TAQUI","GRAV_EXTRE","GRAV_HIPOT","GRAV_HEMAT","GRAV_MELEN","GRAV_METRO", "GRAV_SANG", "GRAV_AST", "GRAV_MIOC" ,"GRAV_CONSC" ,"GRAV_ORGAO","MANI_HEMOR", "EPISTAXE" ,"GENGIVO" ,"METRO" ,"PETEQUIAS", "HEMATURA" ,"SANGRAM" ,"LACO_N", "PLASMATICO" ,"EVIDENCIA" ,"PLAQ_MENOR" ,"CON_FHD", "COMPLICA")
+factorVars<-c("SG_UF_NOT","ID_MUNICIP","ID_REGIONA","ID_UNIDADE","CS_SEXO","CS_GESTANT","CS_RACA","CS_ESCOL_N","ID_DISTRIT","ID_BAIRRO","ID_GEO1","NU_DDD_TEL","CS_ZONA","FEBRE","MIALGIA","CEFALEIA","NAUSEA","EXANTEMA","DOR_COSTAS","CONJUNTVIT","ARTRITE","ARTRALGIA","PETEQUIA_N","LEUCOPENIA","LACO","DOR_RETRO","DIABETES","HEMATOLOG","HEPATOPAT","RENAL", "HIPERTENSA","ACIDO_PEPT","AUTO_IMUNE","RES_CHIKS1","RES_CHIKS2","RESUL_PRNT","RESUL_SORO","RESUL_NS1","RESUL_VI_N","RESUL_PCR_","SOROTIPO","HISTOPA_N","IMUNOH_N","HOSPITALIZ","MUNICIPIO","HOSPITAL", "DDD_HOSP","TPAUTOCTO","COUFINF","COPAISINF","CO_BAINF","CLASSI_FIN","CRITERIO","DOENCA_TRA","EVOLUCAO","ALRM_HIPOT","ALRM_PLAQ","ALRM_VOM","ALRM_SANG","ALRM_HEMAT","ALRM_ABDOM","ALRM_LETAR","ALRM_HEPAT","ALRM_LIQ","GRAV_PULSO","GRAV_CONV","GRAV_ENCH","GRAV_INSUF","GRAV_TAQUI","GRAV_EXTRE","GRAV_HIPOT","GRAV_HEMAT","GRAV_MELEN","GRAV_METRO", "GRAV_SANG", "GRAV_AST", "GRAV_MIOC" ,"GRAV_CONSC" ,"GRAV_ORGAO","MANI_HEMOR", "EPISTAXE" ,"GENGIVO" ,"METRO" ,"PETEQUIAS", "HEMATURA" ,"SANGRAM" ,"LACO_N", "PLASMATICO" ,"EVIDENCIA" ,"PLAQ_MENOR" ,"CON_FHD", "COMPLICA")
+tableOne <- CreateTableOne(vars = vars, factorVars=factorVars,strata = "year", data = denv)
 table1 <- print(tableOne, exact=vars, nonnormal=vars, quote = FALSE, noSpaces = TRUE, printToggle = FALSE)
 write.csv(table1, file = "table1.csv")
--- zika grenada/zika_grenada.R
+++ zika grenada/zika_grenada.R
@@ -1,75 +1,100 @@
-#install.packages(c("REDCapR", "tableone")
-library(data.table)
+# packages ----------------------------------------------------------------
 library(tableone)
-library(plyr)
 library(REDCapR)
-library(lubridate)
-library(ggplot2)
-library(plotly)
+# get data ----------------------------------------------------------------
+
 setwd("C:/Users/amykr/Box Sync/Amy Krystosik's Files/zika study- grenada")
 Redcap.token <- readLines("Redcap.token.zika.txt") # Read API token from folder
 REDcap.URL  <- 'https://redcap.stanford.edu/api/'
-
-
-#export data from redcap to R (must be connected via cisco VPN)
 #ds <- redcap_read(  redcap_uri  = REDcap.URL,  token   = Redcap.token,  batch_size = 100)$data
-attach(ds)
-preg_cohort<-ds
-#preg_cohort <- read.csv("Zika Results .csv")
-preg_cohort$ID.Code<-as.factor(preg_cohort$ID.Code)
-preg_cohort$mom_id_orig_study
-table(preg_cohort$interpreted_colors.1)
-
-merged<-data.table(ds, key="mom_id_orig_study")[
-  data.table(preg_cohort, key="ID.Code"),
-  allow.cartesian=TRUE
-  ]
-
-delivery_date <- ymd(as.character(ds$delivery_date ))
-delivery_date[ds$delivery_date=="2007-01-15"]<-"2017-01-15"
-prenancy_date<-ds$delivery_date - 280
-
-duration <- 280
-zika_start<- ymd(as.character("2016-06-12"))
-zika_end <- ymd(as.character("2016-10-01"))
-
-f <- "REDCap_export_june19.csv"
-write.csv(as.data.frame(ds), f )
-table(redcap_event_name)
-#split data into mom and child then remerge by id
+table(ds$redcap_event_name)
+# #split data into mom and child then remerge by id ----------------------------------------------------------------
 mom<-subset(ds, redcap_event_name=="mother_arm_1")
 mom <- Filter(function(mom)!all(is.na(mom)), mom)
 
 child<-subset(ds, redcap_event_name=="child_arm_1")
 child <- Filter(function(child)!all(is.na(child)), child)
 total <- merge(mom, child, by="mother_record_id")
-ds<-total
-attach(ds)
-#We will want to know how many kids were Zika exposed (8/206. 7/8 confirmed) 
-table(ever_had_zikv, confirmed_blood_test)
-table(pregnant, confirmed_blood_test) #kids were Zika exposed (8/206. 7/8 confirmed) 
+table(total$redcap_event_name.x)
+table(total$redcap_event_name.y)
 
+# pregnant and symptomatic ------------------------------------------------
 # what the child outcomes were in asymptomatic and symptomatic pregnant cases. 
 #By child outcome I mean child anthropometrics and PE findings.
-symptom_sum<-rowSums(ds[, grep("\\bsymptoms___.", names(ds))])
-table(symptom_sum)
-symptoms<-ds[ , grepl( "symptoms___" , names(ds) ) ]
-ds$symptom_sum <- as.integer(rowSums(ds[ , grep("symptoms___" , names(ds))]))
-table(ds$symptom_sum)
+total$symptom_sum <- as.integer(rowSums(total[ , grep("symptoms___" , names(total))]))
+table(total$symptom_sum)
 
-symptomatic<-NA
-ds <- within(ds, symptomatic[ds$symptom_sum>0] <- 1)
-ds <- within(ds, symptomatic[ds$symptom_sum==0] <- 0)
-table(ds$symptomatic, ds$pregnant) # symptomatic & pregnant n = 7; non-sympomatic & pregnant n = 1
-t<-with(ds, table(ds$symptomatic, ds$pregnant))
-t <- prop.table(t, margin = 1)
-t
-write.table(t, file = "test.txt", sep = ",", quote = FALSE, row.names = F)
+table(total$preg_f.x)
+table(total$pregnant)
+total$pregnant_cat<-ifelse(is.na(total$preg_f.x),1,total$pregnant)
+table(total$pregnant_cat)
+
+total <- within(total, symptomatic[total$symptom_sum>0] <- 1)
+total <- within(total, symptomatic[total$symptom_sum==0] <- 0)
+table(total$symptomatic, total$pregnant) # symptomatic & pregnant n = 31; non-sympomatic & pregnant n = 2
+table(total$symptomatic, total$pregnant_cat) # symptomatic & pregnant n = 31; non-sympomatic & pregnant n = 2
+table(total$symptomatic) # symptomatic & pregnant n = 7; non-sympomatic & pregnant n = 1
+table(total$pregnant) # symptomatic & pregnant n = 7; non-sympomatic & pregnant n = 1
+table(total$pregnant) # symptomatic & pregnant n = 7; non-sympomatic & pregnant n = 1
+table(total$trimester)
+
+library(expss)
+cro(total$pregnant, total$trimester)
+cro_cpct(total$trimester, list(total()))
+
+# pgold results baby -----------------------------------------------------------
+cro(total$result_zikv_igg_pgold.y)
+(3/388)*100
+cro(total$result_denv_igg_pgold.y)
+(31/388)*100
+cro(total$result_denv_igg_pgold.y,total$result_zikv_igg_pgold.y)
+
+# microcephaly ------------------------------------------------------------
+total$microcephaly <- ifelse(total$zhc < -3.0, 2, ifelse(total$zhc < -2.0, 1, 0))
+table(total$microcephaly)
+
+# pcr moms ----------------------------------------------------------------
+total$result_denv_pcr_mom<-NA
+total <- within(total, result_denv_pcr_mom[total$result_denv_urine_mom==0] <- 0)
+total <- within(total, result_denv_pcr_mom[total$result_denv_serum_mom==0] <- 0)
+total <- within(total, result_denv_pcr_mom[total$result_denv_urine_mom==1] <- 1)
+total <- within(total, result_denv_pcr_mom[total$result_denv_serum_mom==1] <- 1)
+cro(total$result_denv_pcr_mom)
+cro(total$result_denv_pcr_mom, total$symptomatic)
+
+
+# pgold moms ----------------------------------------------------------------
+cro(total$result_zikv_igg_pgold.x)
+cro(total$result_denv_igg_pgold.x)
+cro(total$result_denv_igg_pgold.x,total$result_zikv_igg_pgold.x)
+
+cro(total$result_zikv_igg_pgold.x)
+cro(total$result_denv_igg_pgold.x)
+
+cro(total$result_denv_pcr_mom, total$result_denv_igg_pgold.x)
+
+cro(total$result_denv_pcr_mom, total$result_denv_igg_pgold.x)
+
+cro(result_avidity_zikv_igg_pgold)
+cro(result_avidity_denv_igg_pgold)
+
+
+# mother baby exopsure ----------------------------------------------------
+cro(total$result_zikv_igg_pgold.x,total$result_zikv_igg_pgold.y)
+
+cro(total$result_denv_igg_pgold.x,total$result_denv_igg_pgold.y)
+
+
+
+
+
+# old code not used. ----------------------------------------------------
+
 
 ## List numerically coded categorical variables
-ds$parish<-as.factor(ds$parish)
-ds$race<-as.factor(ds$race)
-ds$race<-as.factor(ds$gender)
+total$parish<-as.factor(total$parish)
+total$race<-as.factor(total$race)
+total$race<-as.factor(total$gender)
 factorVars <- c("parish","race", "gender",  "child_delivery", "delivery_type", "outcome_of_delivery",
                 "opv_vaccine", "vac_utd", 
                 "color___1", "color___2", "color___3", "color___4", "color___5", "color___6",
@@ -86,12 +111,11 @@ factorVars <- c("parish","race", "gender",  "child_delivery", "delivery_type", "
 ## Create a variable list. Use dput(names(pbc))
 vars <- c("mean_weight","mean_length","mean_hc","temperature","heart_rate","resp_rate", "parish","race", "gender", "apgar_one", "apgar_ten", "opv_vaccine", "vac_utd",  "temperature", "heart_rate", "resp_rate", "color___1", "color___2", "color___3", "color___4", "color___5", "color___6", "cry", "tone", "moving_limbs", "ant_fontanelle", "sutures", "facial_dysmoph", "cleft", "red_reflex", "cap_refill", "heart_sounds", "murmur", "breath_sounds", "breath_noises___1", "breath_noises___2", "breath_noises___3", "breath_noises___0", "breath_noises___99", "resp_effort___0", "resp_effort___1", "resp_effort___2", "resp_effort___99", "bowel_sounds", "hernia", "organomegaly___0", "organomegaly___1", "organomegaly___2", "organomegaly___99", "testes", "patent_anus", "hip_manouver", "hip_creases", "femoral_pulse", "scoliosis", "sacral_dimple", "moro", "grasp", "suck", "plantar_reflex", "galant_reflex", "ever_had_dengue")
 
-cases<-subset(ds, pregnant=="1")
+cases<-subset(total, pregnant=="1")
 ## Create Table 1 stratified by trt (omit strata argument for overall table)
 tableOne <- CreateTableOne(vars = vars, factorVars = factorVars, strata = "symptomatic", data = cases)
 ## Just typing the object name will invoke the print.TableOne method
 ## Tests are by oneway.test/t.test for continuous, chisq.test for categorical
-tableOne
 tableOne$CatTable
 summary(tableOne)
 print(tableOne, 
@@ -109,8 +133,8 @@ print(tableOne,
 #Multilevel modeling (MLM)[72] will be used to adjust for the effects of multiple host factors. 
 #All tests will be two-sided and performed at significance level 0.05. 
 #We will test for differences in binomial proportion, use chi-square tests for categorical predictors, and simple logistic regression for numeric predictors.
-table(ds$hospitalized_chikv)
-table(ds$hospitalized_denv)
+table(total$hospitalized_chikv)
+table(total$hospitalized_dengue)
 
 #we don't have have the mtct lab results yet. can't do this. 
 
@@ -119,12 +143,4 @@ table(ds$hospitalized_denv)
 #Assuming 250 (50%) of the 500 of the pregnant women will be ZIKV infected, 
 #20% of whom will be symptomatic with an estimated 50% MTCT rate, we will have power of 90% to detect 
 #a difference if the rate is 25% for asymptomatic mothers and 75% if the rate is 30% for asymptomatics.
-                                                                                                                                                                                                   
-f <- "ds.csv"
-write.csv(as.data.frame(ds), f )
-
-
-tetracore
-pos
-maybe
-neg
+write.csv(total, "total.csv")
