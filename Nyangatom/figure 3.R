2fd993e5ea4aa06918f25c7848f0c8ff7d87ae3c
amyrobyn <amykrystosik@gmail.com>
2018-05-04 15:24:42 -0700

    fig 3 obs.

--- Nyangatom/figure 3.R
+++ Nyangatom/figure 3.R
@@ -12,12 +12,12 @@ nyg+geom_smooth(aes(height_cm,child_weight_kg),level=.95,size=2,formula=y~x,meth
 
 
 ggplot(ds,aes(height_cm,child_weight_kg))+
-  geom_point(aes(height_cm,child_weight_kg))+
+  geom_point(aes(height_cm,child_weight_kg), size = 2, alpha = .5)+
   facet_grid(.~sex_child)+
   geom_smooth(aes(height_cm,child_weight_kg, colour = "black"),level=.95,size=2,formula=y~x,method="loess") +
-  geom_line(aes(who_height,who_med, colour="blue"),size=2 ,alpha=.5) +
+  geom_line(aes(who_height,who_med, colour="blue"),size=2 ,alpha=.5,fill="blank") +
   geom_line(aes(who_height,ds$who_3sd, colour="red"),size=2,alpha=.5 ) +
   labs(title ="", x = "Height (cm)", y = "Weight (Kg)")+
-  scale_color_discrete(name = "Y series", labels = c("Loess smoothed Regression \n 95% CI of Nyangatom median", "WHO Median", "WHO -3 SD"))+
+  scale_color_discrete(name = "Y series", labels = c("Loess smoothed Regression \n 95% CI of Nyangatom Observations", "WHO Median", "WHO -3 SD"))+
   theme_classic(base_size = 30)+ theme(legend.position="bottom")
 
