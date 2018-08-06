e5e74cf74c1f8378e2e5dc9fa2d286fb4b4d772d
amyrobyn <amykrystosik@gmail.com>
2018-08-06 11:40:03 -0700

    look for results from google missing in redcap

--- R01/Data Cleaning & merging/google drive to redcap elisas.R
+++ R01/Data Cleaning & merging/google drive to redcap elisas.R
@@ -3,3 +3,7 @@ MissingELISAs <- read_csv("C:/Users/amykr/Box Sync/Amy Krystosik's Files/Data Ma
 MissingELISAs<-MissingELISAs[which(MissingELISAs$redcap_event_name!="patient_informatio_arm_1"&MissingELISAs$redcap_event_name!="visit_u24_arm_1"),]
 MissingELISAs<-MissingELISAs[which(is.na(MissingELISAs$result_igg_chikv_stfd) | is.na(MissingELISAs$result_igg_chikv_stfd)),]
 MissingELISAs<-MissingELISAs[which(!is.na(MissingELISAs$interview_date) | !is.na(MissingELISAs$interview_date_aic)| (MissingELISAs$serum_sample_num>0 & !is.na(MissingELISAs$serum_sample_num)) ),]
+
+
+googledoc <- read_csv("C:/Users/amykr/Box Sync/Amy Krystosik's Files/Data Managment/redcap/MissingELISACleanUp.csv")
+
