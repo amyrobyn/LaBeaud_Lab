77388df49f6857998447494f237dae16b5f6cc90
amyrobyn <amykrystosik@gmail.com>
2020-04-13 18:31:50 -0700

    PZ182 algorithm update
    change order of algorithm to account for pcr positive but initial
    negative and fu positive (PZ182)

--- zika grenada/maternal_exposure_strata_3.R
+++ zika grenada/maternal_exposure_strata_3.R
@@ -1,6 +1,6 @@
 ds2$zikv_exposed_mom<-NA
 ds2 <- within(ds2, zikv_exposed_mom[ds2$result_zikv_igg_pgold.mom=="Negative"|ds2$result_zikv_igg_pgold_fu.mom=="Negative"] <- "mom_zikv_Unexposed_during_pregnancy")
-ds2 <- within(ds2, zikv_exposed_mom[ds2$pcr_positive_zikv_mom=="Positive"|ds2$result_zikv_igg_pgold.mom=="Positive"] <- "mom_ZIKV_Exposed_during_pregnancy")
 ds2 <- within(ds2, zikv_exposed_mom[(ds2$result_zikv_igg_pgold.mom=="Negative"|is.na(ds2$result_zikv_igg_pgold.mom)) & ds2$result_zikv_igg_pgold_fu.mom=="Positive"] <- "mom_ZIKV_Exposure_possible_during_pregnancy")
+ds2 <- within(ds2, zikv_exposed_mom[ds2$pcr_positive_zikv_mom=="Positive"|ds2$result_zikv_igg_pgold.mom=="Positive"] <- "mom_ZIKV_Exposed_during_pregnancy")
 
-
+table(ds2$zikv_exposed_mom)
